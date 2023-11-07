f5fef74a84c6ae7c4e185bf7d2bc8939a180604c
Allen Chou <f1470891079@gmail.com>
2023-11-07 17:44:53 +0800

    Test LocalPath methods initiation and samefile

--- tests/test_local_path_base.py
+++ tests/test_local_path_base.py
@@ -1,3 +1,4 @@
+import pathlib
 import shutil
 from typing import TYPE_CHECKING
 
@@ -11,10 +12,17 @@ if TYPE_CHECKING:
 
 @pytest.fixture(scope="module")
 def temp_dir(tmp_path_factory: "TempPathFactory"):
-    temp_dir = tmp_path_factory.mktemp("data")
+    temp_dir = tmp_path_factory.mktemp("test_local_path_base")
     yield temp_dir
     shutil.rmtree(temp_dir)
 
 
-def test_local_path_basic_operations(temp_dir):
-    pass
+def test_local_path_basic_operations(temp_dir: "pathlib.PosixPath"):
+    # Initiation
+    path = LocalPath(temp_dir.as_uri())
+    path.mkdir(exist_ok=True)
+
+    # test samefile
+    path2 = LocalPath(temp_dir.as_uri())
+    assert path.samefile(path2)
+    assert not path.samefile(LocalPath("file:///tmp"))
